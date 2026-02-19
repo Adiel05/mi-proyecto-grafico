6f7aa060832f2225db407bc5b07fa7d3ec2895a1
Adiel <noelpabonnina@gmail.com>
2026-02-18 21:18:13 -0400

    1er avance

--- src/app/editor-filtro/editor-filtro.ts
+++ src/app/editor-filtro/editor-filtro.ts
@@ -1,4 +1,4 @@
-import { Component , signal,computed} from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 
 @Component({
   selector: 'app-editor-filtro',
@@ -7,18 +7,26 @@ import { Component , signal,computed} from '@angular/core';
   styleUrl: './editor-filtro.scss',
 })
 export class EditorFiltroComponent {
-  brillo= signal(100);
+  brillo = signal(100);
   contraste = signal(100);
   blur = signal(0);
+  blancoNegro = signal(false);
 
-  filtroScss = computed(()=>{
-    return `brightness(${this.brillo()}%) contrast(${this.contraste()}%) blur(${this.blur()}px)`;
-  })
+  filtroScss = computed(() => {
+    const base = `brightness(${this.brillo()}%) contrast(${this.contraste()}%) blur(${this.blur()}px)`;
+    const escala = this.blancoNegro() ? ' grayscale(100%)' : '';
+    return base + escala;
+  });
 
-  actualizar (prop: string, evento: Event){
+  actualizar(prop: string, evento: Event) {
     const valor = (evento.target as HTMLInputElement).value;
     if (prop === 'brillo') this.brillo.set(+valor);
     if (prop === 'contraste') this.contraste.set(+valor);
     if (prop === 'blur') this.blur.set(+valor);
   }
+
+  toggleBlancoNegro() {
+    this.blancoNegro.update(v => !v);
+  }
 }
+
